a90fec6adaa6f5fc3455c3fe78be046f630b6b5b
Astra <astrapch4@gmail.com>
2025-12-22 16:17:01 +0100

    new

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import osintRoutes from "./routes/osint.routes.js";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
